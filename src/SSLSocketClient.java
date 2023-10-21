3bab789849c6cb9ad26f536b8a8345921f523970
chensy618 <146075999+chensy618@users.noreply.github.com>
2023-10-21 17:55:20 +0200

    Add SSL SOCKET bidirectional authentication

---
+++ src/SSLSocketClient.java
@@ -0,0 +1,59 @@
+package src;
+
+import javax.net.ssl.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.UnknownHostException;
+import java.security.KeyStore;
+
+
+public class SSLSocketClient {
+    private static String kpath = "D:\\Github\\Data_Security\\keytool\\sslclient.keystore";
+    private static String tpath = "D:\\Github\\Data_Security\\keytool\\sslclient.keystore";
+    private static char[] password = "123456789".toCharArray();
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SSLContext context = null;
+        try {
+            KeyStore ks = KeyStore.getInstance("JKS");
+            ks.load(new FileInputStream(kpath), password);
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, password);
+            KeyManager[] km = kmf.getKeyManagers();
+            KeyStore ts = KeyStore.getInstance("JKS");
+            ts.load(new FileInputStream(tpath), password);
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ts);
+            TrustManager[] tm = tmf.getTrustManagers();
+            context = SSLContext.getInstance("SSL");
+            context.init(km, tm, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        SSLSocketFactory ssf = context.getSocketFactory();
+        try {
+            SSLSocket ss = (SSLSocket) ssf.createSocket("localhost", 8000);
+            //ss.setNeedClientAuth(true);
+            //ss.setUseClientMode(true);
+            //ss.setWantClientAuth(true);
+            System.out.println("Client : I'm client, requesting server ....");
+            ObjectInputStream br = new ObjectInputStream(ss.getInputStream());
+            try {
+                System.out.println(br.readObject());
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            br.close();
+            ss.close();
+            System.out.println("Client: Authentication success for server reply");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/SSLSocketServer.java
@@ -0,0 +1,57 @@
+package src;
+
+import javax.net.ssl.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.security.KeyStore;
+
+
+
+public class SSLSocketServer {
+    private static String kpath = "D:\\Github\\Data_Security\\keytool\\sslserver.keystore";
+    private static String tpath = "D:\\Github\\Data_Security\\keytool\\sslserver.keystore";
+    private static char[] password = "123456789".toCharArray();
+    public static void main(String[] args) {
+        boolean flag = true;
+        SSLContext context = null;
+        try {
+            KeyStore ks = KeyStore.getInstance("JKS");
+            ks.load(new FileInputStream(kpath), password);
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, password);
+            KeyManager[] km = kmf.getKeyManagers();
+            KeyStore ts = KeyStore.getInstance("JKS");
+            ts.load(new FileInputStream(tpath), password);
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ts);
+            TrustManager[] tm = tmf.getTrustManagers();
+            context = SSLContext.getInstance("SSL");
+            context.init(km, tm, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        SSLServerSocketFactory ssf = (SSLServerSocketFactory) context.getServerSocketFactory();
+        try {
+            SSLServerSocket ss = (SSLServerSocket) ssf.createServerSocket(8000);
+            //ss.setNeedClientAuth(true);
+            //ss.setUseClientMode(true);
+            //ss.setWantClientAuth(true);
+            System.out.println("Server: waiting for the connection....");
+            while (flag) {
+                Socket s = ss.accept();
+                System.out.println("Server: received the request from client");
+                ObjectOutputStream os = new ObjectOutputStream(s.getOutputStream());
+                os.writeObject("Reply from Server : Authentication success for client request");
+                os.flush();
+                os.close();
+                System.out.println();
+                s.close();
+            }
+            ss.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
